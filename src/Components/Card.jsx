055be84df34311de925fc718635c7e0dbb9a01a8
SajeedAninda <sajaninda@gmail.com>
2024-06-24 15:37:38 +0600

    Sorted all texts

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,22 +4,26 @@ import productImg from "../assets/productImg.jpg"
 const Card = () => {
     return (
         <div className='bg-[#f2ebe3] flex justify-center items-center'>
-            <div className='w-[50%] rounded-2xl my-20 flex'>
-                <div className="imgDiv flex-1">
-                    <img className='w-full object-cover rounded-tl-2xl rounded-bl-2xl' src={productImg} alt="" />
+            <div className='w-[50%] rounded-2xl my-20 flex h-[70vh] justify-center items-center'>
+                <div className="imgDiv w-[50%] h-full">
+                    <img className='w-full object-cover rounded-tl-2xl rounded-bl-2xl h-full' src={productImg} alt="" />
                 </div>
 
 
-                <div className="textDiv flex-1 bg-white p-8">
+                <div className="textDiv w-[50%] bg-white p-8 rounded-tr-2xl rounded-br-2xl h-full flex flex-col justify-evenly">
                     <h3 className='text-[#6C7289] text-base'>PERFUME</h3>
 
-                    <h1 className='text-[#1C232B] text-3xl font-bold'>Gabrielle Essence Eau De Parfum</h1>
+                    <h1 className='text-[#1C232B] text-3xl font-bold'>Gabrielle <br /> Essence Eau <br /> De Parfum</h1>
 
                     <p className='text-[#6C7289] text-base'>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.
                     </p>
 
-                    <div>
+                    <div className='flex justify-between items-center w-[70%]'>
                         <p className='text-[#3C8067] text-3xl font-bold'>$149.99</p>
+
+                        <p className='text-[#6C7289] text-base line-through'>
+                            $169.99
+                        </p>
                     </div>
 
                 </div>
