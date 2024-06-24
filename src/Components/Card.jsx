ea0d8d02ac634ded077dd306fa14f3db88dda1cc
SajeedAninda <sajaninda@gmail.com>
2024-06-24 16:08:51 +0600

    Completed for desktop view

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -12,22 +12,22 @@ const Card = () => {
 
 
                 <div className="textDiv w-[50%] bg-white p-8 rounded-tr-2xl rounded-br-2xl h-full flex flex-col justify-between">
-                    <h3 className='text-[#6C7289] text-base'>PERFUME</h3>
+                    <h3 className='text-[#6C7289] text-base font-montserrat tracking-widest'>PERFUME</h3>
 
-                    <h1 className='text-[#1C232B] text-3xl font-bold'>Gabrielle <br /> Essence Eau <br /> De Parfum</h1>
+                    <h1 className='text-[#1C232B] text-3xl font-bold font-fraunces'>Gabrielle <br /> Essence Eau <br /> De Parfum</h1>
 
-                    <p className='text-[#6C7289] text-base'>A floral, solar and voluptuous interpretation composed by <br /> Olivier Polge, Perfumer-Creator for <br /> the House of CHANEL.
+                    <p className='text-[#6C7289] text-[14px] font-montserrat'>A floral, solar and voluptuous interpretation composed by <br /> Olivier Polge, Perfumer-Creator for <br /> the House of CHANEL.
                     </p>
 
                     <div className='flex justify-between items-center w-[70%]'>
-                        <p className='text-[#3C8067] text-3xl font-bold'>$149.99</p>
+                        <p className='text-[#3C8067] text-3xl font-bold font-fraunces'>$149.99</p>
 
-                        <p className='text-[#6C7289] text-base line-through'>
+                        <p className='text-[#6C7289] text-base line-through font-montserrat'>
                             $169.99
                         </p>
                     </div>
 
-                    <button className='w-full flex justify-center gap-2items-center px-3 py-3 bg-[#3C8067] text-white hover:bg-[#265040] rounded-2xl font-semibold'>
+                    <button className='w-full flex justify-center gap-2items-center px-3 py-3 bg-[#3C8067] text-white hover:bg-[#265040] rounded-2xl font-semibold font-montserrat'>
                         <img src={iconCartSvg} alt="" />
                         Add to Cart
                     </button>
