9a8d1733dd336922ff9b8abcdd6b1641951b7891
SajeedAninda <sajaninda@gmail.com>
2024-06-24 15:42:55 +0600

    Added a button

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import productImg from "../assets/productImg.jpg"
+import iconCartSvg from "../assets/icon-cart.svg"
 
 const Card = () => {
     return (
@@ -10,12 +11,12 @@ const Card = () => {
                 </div>
 
 
-                <div className="textDiv w-[50%] bg-white p-8 rounded-tr-2xl rounded-br-2xl h-full flex flex-col justify-evenly">
+                <div className="textDiv w-[50%] bg-white p-8 rounded-tr-2xl rounded-br-2xl h-full flex flex-col justify-between">
                     <h3 className='text-[#6C7289] text-base'>PERFUME</h3>
 
                     <h1 className='text-[#1C232B] text-3xl font-bold'>Gabrielle <br /> Essence Eau <br /> De Parfum</h1>
 
-                    <p className='text-[#6C7289] text-base'>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.
+                    <p className='text-[#6C7289] text-base'>A floral, solar and voluptuous interpretation composed by <br /> Olivier Polge, Perfumer-Creator for <br /> the House of CHANEL.
                     </p>
 
                     <div className='flex justify-between items-center w-[70%]'>
@@ -25,6 +26,11 @@ const Card = () => {
                             $169.99
                         </p>
                     </div>
+
+                    <button className='w-full flex justify-center gap-2items-center px-3 py-3 bg-[#3C8067] text-white hover:bg-[#265040] rounded-2xl font-semibold'>
+                        <img src={iconCartSvg} alt="" />
+                        Add to Cart
+                    </button>
 
                 </div>
             </div>
