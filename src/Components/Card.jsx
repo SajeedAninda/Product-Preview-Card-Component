38996b7831c63b2c29ae59fe1f05d3527d7c368d
SajeedAninda <sajaninda@gmail.com>
2024-06-24 16:25:56 +0600

    Minor adjustments

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -7,9 +7,9 @@ const Card = () => {
         <div className='bg-[#f2ebe3] flex justify-center items-center h-full lg:h-screen'>
             <div className='w-[90%] md:w-[70%] lg:w-[50%] rounded-2xl my-20 flex flex-col lg:flex-row h-full lg:h-[70vh]'>
                 <div className="imgDiv w-full lg:w-[50%] h-full">
-                    <img className='w-full h-full object-cover rounded-tl-2xl rounded-tr-2xl lg:rounded-tl-2xl lg:rounded-bl-2xl' src={productImg} alt="" />
+                    <img className='w-full h-full object-cover rounded-tl-2xl rounded-tr-2xl lg:rounded-tr-none lg:rounded-bl-2xl' src={productImg} alt="" />
                 </div>
-                <div className="textDiv w-full lg:w-[50%] bg-white p-8 rounded-br-2xl rounded-bl-2xl lg:rounded-tr-2xl lg:rounded-br-2xl h-full flex flex-col justify-between space-y-6 lg:space-y-0">
+                <div className="textDiv w-full lg:w-[50%] bg-white p-8 rounded-br-2xl rounded-bl-2xl lg:rounded-tr-2xl lg:rounded-bl-none h-full flex flex-col justify-between space-y-6 lg:space-y-0">
                     <h3 className='text-[#6C7289] text-base font-montserrat tracking-widest'>PERFUME</h3>
                     <h1 className='text-[#1C232B] text-3xl font-bold font-fraunces'>Gabrielle <br /> Essence Eau <br /> De Parfum</h1>
                     <p className='text-[#6C7289] text-[14px] font-montserrat'>
